b0259fcddb9ff02416535ead62736f9606c630d1
Gonzalo Azaldegi <me@azaldev.com>
2023-11-05 15:48:43 +0100

    Changed colors

--- src/config/colors.js
+++ src/config/colors.js
@@ -2,7 +2,7 @@
 export default{
     global: {
         '--color-tone-50': '#f3f7fb',
-        '--color-tone-100': '#e3ecf6',
+        '--color-tone-100': '#222',
         '--color-tone-200': '#cddff0',
         '--color-tone-300': '#aacae6',
         '--color-tone-400': '#82afd8',
@@ -11,6 +11,6 @@ export default{
         '--color-tone-700': '#4669af',
         '--color-tone-800': '#3e578f',
         '--color-tone-900': '#364972',
-        '--color-tone-950': '#242e47',
+        '--color-tone-950': '#151515',
     }
 }
